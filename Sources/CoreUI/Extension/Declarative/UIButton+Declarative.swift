bebce491c1f955e170921b095e6f0459a5ab568b
Joshua T. Wood <joshuatw@gmail.com>
2022-05-05 23:57:33 +0200

    Add declarative methods for UIButton

--- Sources/CoreUI/Extension/Declarative/UIButton+Declarative.swift
+++ Sources/CoreUI/Extension/Declarative/UIButton+Declarative.swift
@@ -20,4 +20,24 @@ extension UIButton {
         return self
     }
     
+    @discardableResult
+    public func settingTitleFont(_ font: UIFont) -> Self {
+        self.titleLabel?.font = font
+        return self
+    }
+    
+    @discardableResult
+    public func addingTarget(_ target: Any?, action: Selector, for event: UIControl.Event) -> Self {
+        self.addTarget(target, action: action, for: event)
+        return self
+    }
+    
+    @available(iOS 14.0, *)
+    @discardableResult
+    public func addingAction(_ action: UIAction, for event: UIControl.Event) -> Self {
+        self.addAction(action, for: event)
+        return self
+    }
+
+    
 }
