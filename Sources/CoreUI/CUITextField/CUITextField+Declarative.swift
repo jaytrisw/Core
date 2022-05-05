fe849fcd6b1040e6c6ae0bbd40d40295a0592111
Joshua T. Wood <joshuatw@gmail.com>
2022-05-06 00:07:30 +0200

    Update access levels

--- Sources/CoreUI/CUITextField/CUITextField+Declarative.swift
+++ Sources/CoreUI/CUITextField/CUITextField+Declarative.swift
@@ -1,51 +1,51 @@
 import UIKit
 
-extension CUITextField {
+public extension CUITextField {
     
     @discardableResult
-    public func settingTitleLabelText(_ text: String) -> Self {
+    func settingTitleLabelText(_ text: String) -> Self {
         self.setTitleLabelText(text)
         return self
     }
     
     @discardableResult
-    public func settingModel(_ model: Model) -> Self {
+    func settingModel(_ model: Model) -> Self {
         self.setModel(model)
         return self
     }
     
     @discardableResult
-    public func settingTextAlignment(_ textAlignment: NSTextAlignment) -> Self {
+    func settingTextAlignment(_ textAlignment: NSTextAlignment) -> Self {
         self.setTextAlignment(textAlignment)
         return self
     }
     
     @discardableResult
-    public func settingTextContentType(_ textContentType: UITextContentType) -> Self {
+    func settingTextContentType(_ textContentType: UITextContentType) -> Self {
         self.setTextContentType(textContentType)
         return self
     }
     
     @discardableResult
-    public func settingAutocorrectionType(_ autocorrectionType: UITextAutocorrectionType) -> Self {
+    func settingAutocorrectionType(_ autocorrectionType: UITextAutocorrectionType) -> Self {
         self.setAutocorrectionType(autocorrectionType)
         return self
     }
     
     @discardableResult
-    public func settingKeyboardAppearance(_ keyboardAppearance: UIKeyboardAppearance) -> Self {
+    func settingKeyboardAppearance(_ keyboardAppearance: UIKeyboardAppearance) -> Self {
         self.setKeyboardAppearance(keyboardAppearance)
         return self
     }
     
     @discardableResult
-    public func settingKeyboardType(_ keyboardType: UIKeyboardType) -> Self {
+    func settingKeyboardType(_ keyboardType: UIKeyboardType) -> Self {
         self.setKeyboardType(keyboardType)
         return self
     }
     
     @discardableResult
-    public func settingAutocapitalizationType(_ autocapitalizationType: UITextAutocapitalizationType) -> Self {
+    func settingAutocapitalizationType(_ autocapitalizationType: UITextAutocapitalizationType) -> Self {
         self.setAutocapitalizationType(autocapitalizationType)
         return self
     }
